19dbaffc7da71c900225a3bf088f7f7ef9c7d6eb
张俊正 <zhangjunzheng@shein.com>
2023-05-12 22:50:45 +0800

    feat: python formatter

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -9,17 +9,20 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
 
   -- cpp
   b.formatting.clang_format,
+
+  -- python
+  b.formatting.autopep8,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
-}
+})
